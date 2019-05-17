adb3212921bfaabee427bfdc85e69eee63afdff4
Michelle Lam <shelly2.lam@gmail.com>
2019-05-17 03:23:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function sayHiToGrandma(string){
    return "I can't hear you!"
  }
  else if (string === "I love you, Grandma.") {
-   return "I love you, too"
+   return "I love you, too."
  }
 }
 
